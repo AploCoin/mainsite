81be20705a7bb4d57250bc6f5b356cfb4318e120
DoctorEenot <dreenot@gmail.com>
2023-09-10 02:57:25 +0300

    fix config

--- next.config.js
+++ next.config.js
@@ -1,30 +1,36 @@
+const repo = 'MainSite'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-}
-
-module.exports = nextConfig
-
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-
-  module.exports = {
     output: 'export',
     assetPrefix: assetPrefix,
     basePath: basePath,
-  }
 }
 
 module.exports = nextConfig
+
+
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+// let assetPrefix = ''
+// let basePath = '/'
+
+// if (isGithubActions) {
+//   // trim off `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+//   assetPrefix = `/${repo}/`
+//   basePath = `/${repo}`
+
+//   module.exports = {
+//     output: 'export',
+//     assetPrefix: assetPrefix,
+//     basePath: basePath,
+//   }
+// }
+
+// module.exports = nextConfig
 
 
