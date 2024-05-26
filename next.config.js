795e8519ca9c4d21414df28bb0f36e3a61effced
YeahNotSewerSide <47860375+DoctorEenot@users.noreply.github.com>
2024-05-27 01:44:31 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,15 +14,16 @@ module.exports = withNextIntl(nextConfig);
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = '/'
+//let assetPrefix = ''
+//let basePath = '/'
 
 if (isGithubActions) {
+ 
 //   // trim off `<owner>/`
    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
    assetPrefix = `/${repo}/`
-   basePath = `/${repo}`
+   basePath = `/`
    module.exports = {
      output: 'export',
      assetPrefix: assetPrefix,
