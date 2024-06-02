e333a7744d70163dc58a07e886a82ebb52d1b65f
ArtemkaDev <mine38883@gmail.com>
2024-06-02 11:34:04 +0300

    f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-'use client';
-import { useEffect } from "react";
 import { redirect } from "next/navigation";
 
-const supportedLanguages = ['en', 'ru', 'ua'];
-
 export default function RootPage() {
-  useEffect(() => {
-    const langPrefix = navigator.language.split('-')[0];
-
-    if (supportedLanguages.includes(langPrefix)) {
-      redirect(`/${langPrefix}`);
-    } else {
-      redirect('/en');
-    }
-  }, []);
-
-  return null;
+  redirect("/en");
 }
